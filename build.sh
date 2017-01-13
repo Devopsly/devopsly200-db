895b91552fcb777d8a7a6eff4d7151a20a754a47
ramesh <ramesh@nirvaha.com>
2017-01-13 10:12:53 -0800

    debug build

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 buildNumber=${BUILD_NUMBER}
 
-docker rmi -f localhost:5000/devopsly200-db-teststage:$buildNumber
-docker build -t localhost:5000/devopsly200-db-teststage:$buildNumber -f DockerfileTest .
+docker rmi -f localhost:5000/devopsly200-db-teststage
+docker build -t localhost:5000/devopsly200-db-teststage -f DockerfileTest .
 
-docker rmi -f localhost:5000/devopsly200-db-teststage-failed:$buildNumber
-docker build -t localhost:5000/devopsly200-db-teststage-failed:$buildNumber -f DockerfileTest .
+docker rmi -f localhost:5000/devopsly200-db-teststage-failed
+docker build -t localhost:5000/devopsly200-db-teststage-failed -f DockerfileTest .
 
-docker rmi -f localhost:5000/devopsly200-db-stagingstage:$buildNumber
-docker build -t localhost:5000/devopsly200-db-stagingstage:$buildNumber -f DockerfileStaging .
+docker rmi -f localhost:5000/devopsly200-db-stagingstage
+docker build -t localhost:5000/devopsly200-db-stagingstage -f DockerfileStaging .
 
-docker rmi -f localhost:5000/devopsly200-db-stagingstage-failed:$buildNumber
-docker build -t localhost:5000/devopsly200-db-stagingstage-failed:$buildNumber -f DockerfileStaging .
+docker rmi -f localhost:5000/devopsly200-db-stagingstage-failed
+docker build -t localhost:5000/devopsly200-db-stagingstage-failed -f DockerfileStaging .
 
-docker rmi -f localhost:5000/devopsly200-db-productionstage:$buildNumber
-docker build -t localhost:5000/devopsly200-db-productionstage:$buildNumber -f DockerfileProduction .
+docker rmi -f localhost:5000/devopsly200-db-productionstage
+docker build -t localhost:5000/devopsly200-db-productionstage -f DockerfileProduction .
 
-docker rmi -f localhost:5000/devopsly200-db-productionstage-failed:$buildNumber
-docker build -t localhost:5000/devopsly200-db-productionstage-failed:$buildNumber -f DockerfileProduction .
+docker rmi -f localhost:5000/devopsly200-db-productionstage-failed
+docker build -t localhost:5000/devopsly200-db-productionstage-failed -f DockerfileProduction .
