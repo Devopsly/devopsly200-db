e3a0bf0975cc8f85ff4bfc9aed362c6c07b0c401
ramesh <ramesh@nirvaha.com>
2017-01-13 09:31:49 -0800

    bug fix in push

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 buildNumber=${BUILD_NUMBER}
 
-docker push localhost:5000/devopsly200-db-teststage:$buildNumber
-docker push localhost:5000/devopsly200-db-teststage-failed:$buildNumber
-docker push localhost:5000/devopsly200-db-stagingstage:$buildNumber
-docker push localhost:5000/devopsly200-db-stagingstage-failed:$buildNumber
-docker push localhost:5000/devopsly200-db-productionstage:$buildNumber
-docker push localhost:5000/devopsly200-db-productionstage-failed:$buildNumber
+docker push localhost:5000/devopsly200-db-teststage
+docker push localhost:5000/devopsly200-db-teststage-failed
+docker push localhost:5000/devopsly200-db-stagingstage
+docker push localhost:5000/devopsly200-db-stagingstage-failed
+docker push localhost:5000/devopsly200-db-productionstage
+docker push localhost:5000/devopsly200-db-productionstage-failed
